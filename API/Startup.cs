745ab21f1f2a95032c1662af34005ce6be29a871
Youssef TAYEK <yousseftayek@gmail.com>
2021-04-10 16:10:59 +0200

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,6 +34,8 @@ namespace API
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddCors();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -54,8 +56,9 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             app.UseAuthorization();
+            
 
             app.UseEndpoints(endpoints =>
             {
